c4f411b037533208a8127b7ce45c82af65acfbe8
ciaos <lufyhali@gmail.com>
2020-11-03 20:13:23 +0800

    idea第一次提交

---
+++ src/main/java/com/itheima/Hero.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+/**
+ * @Author ciaos
+ * @Date 2020/11/3
+ * @Version 1.8
+ */
+public class Hero {
+    private String name;
+    private String sex;
+    private Integer size;
+}
